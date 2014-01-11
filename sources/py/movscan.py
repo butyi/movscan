07f530f26c20315264932ac4f928d022d0e1169e
Butyi PlusPlus <git@butyi.hu>
2014-01-11 21:09:04 +0100

    flip added, code commented

--- sources/py/movscan.py
+++ sources/py/movscan.py
@@ -1,30 +1,59 @@
+# Copyright: Bencsik János <copyright@butyi.hu>
+# License : WTFPL v2 <http://www.wtfpl.net/txt/copying/>
+
 import picamera
 import time
 import os
 import RPi.GPIO as GPIO ## Import GPIO library
 
+# Create object
 with picamera.PiCamera() as camera:
+
+    # Change folder where I want to save images
     os.chdir('/home/pi/pics')
-    GPIO.setmode(GPIO.BOARD) ## Use board pin numbering
-    GPIO.setup(11, GPIO.IN) ## Setup GPIO Pin 11 to IN
-    n = 0
-    camera.start_preview()
-    camera.resolution = 2592,1944
+    
+    # Board I/O init
+    GPIO.setmode(GPIO.BOARD) # Use board pin numbering
+    GPIO.setup(11, GPIO.IN) # Setup GPIO Pin 11 to shot input
+    GPIO.setup(7, GPIO.IN) # Setup GPIO Pin 7 to watchdog input
+    
+    # Image counter
+    n = 0 
+    
+    # Switch on camera 
+    # (following setting needs active camera)
+    camera.start_preview() 
+
+    # Adjust camera for my environment 
+    #camera.resolution = 2592,1944
     #camera.crop=0.295,0.365,0.5,0.5
-    camera.crop=0.1,0.24,0.48,0.48
-    camera.rotation=180
-    camera.resolution = 800,600
-    preview_fullscreen = True
-    time.sleep(3)
-    while True:
+    camera.crop=0.1,0.24,0.48,0.48 # Crop active CCD part
+    camera.hflip = True # Central mirroring is needed due to optic
+    camera.vflip = True # Central mirroring is needed due to optic
+    camera.resolution = 800,600 # Image size (digit zoom, not resize!)
+    preview_fullscreen = True # To see same what will be saved
+    time.sleep(3) # Wait to camera auto settings
+
+    # Main loop
+    Loop = True
+    while Loop:
+        # Wait for shot imput edge
         GPIO.wait_for_edge(11, GPIO.FALLING)
-        #GPIO.wait_for_edge(11, GPIO.RISING)
+
+        # Take the picture
         camera.capture_sequence((
-                'image%05d.jpg' % n
-                for p in range(1)
-                ), use_video_port=True)
-        print "image %d" % n
-        n = n + 1
-    camera.stop_preview()
+                'image%05d.jpg' % n # Numberred file name for video creation later on
+                for p in range(1) # One picture is saved only
+                ), use_video_port=True) # use_video_port=True speeds up the camera
+        print "image %d" % n # Inform me about operating
+        n = n + 1 # Increase image number
+
+        # Check watchdog input. It is pulse from source reel.
+        # It always makes pulse when the reel is moving.
+        # When we don't detect pulse for 10...15s, 
+        # it is stopped, we can leave the loop by Loop = False.
+
+    # Final actions before quit from scrip
+    camera.stop_preview() # Switch off the camera
     camera.close()
 
