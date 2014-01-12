730be0e767ab1eca6b7a5c3fcf9c2ce3a8385c17
BENCSIK János <git@butyi.hu>
2014-01-12 16:59:41 +0000

    script developed

--- sources/py/movscan.py
+++ sources/py/movscan.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 # Copyright: Bencsik János <copyright@butyi.hu>
 # License : WTFPL v2 <http://www.wtfpl.net/txt/copying/>
 
@@ -22,15 +24,14 @@ with picamera.PiCamera() as camera:
     
     # Switch on camera 
     # (following setting needs active camera)
+    width = 800 # Max resolution: 2592 x 1944
+    camera.resolution = width,(width*3/4) # Image size (digit zoom, not resize!)
     camera.start_preview() 
 
     # Adjust camera for my environment 
-    #camera.resolution = 2592,1944
-    #camera.crop=0.295,0.365,0.5,0.5
-    camera.crop=0.1,0.24,0.48,0.48 # Crop active CCD part
-    camera.hflip = True # Central mirroring is needed due to optic
-    camera.vflip = True # Central mirroring is needed due to optic
-    camera.resolution = 800,600 # Image size (digit zoom, not resize!)
+    camera.crop=0.43,0.26,0.5,0.5 # Crop active CCD part
+    camera.vflip = True # Mirroring is needed due to optic
+    camera.resolution = width,(width*3/4) # Image size (digit zoom, not resize!)
     preview_fullscreen = True # To see same what will be saved
     time.sleep(3) # Wait to camera auto settings
 
@@ -42,7 +43,7 @@ with picamera.PiCamera() as camera:
 
         # Take the picture
         camera.capture_sequence((
-                'image%05d.jpg' % n # Numberred file name for video creation later on
+                'image%05d.jpg' % (n+p) # Numberred file name for video creation later on
                 for p in range(1) # One picture is saved only
                 ), use_video_port=True) # use_video_port=True speeds up the camera
         print "image %d" % n # Inform me about operating
@@ -52,6 +53,8 @@ with picamera.PiCamera() as camera:
         # It always makes pulse when the reel is moving.
         # When we don't detect pulse for 10...15s, 
         # it is stopped, we can leave the loop by Loop = False.
+        #if n>=1:
+        #    break
 
     # Final actions before quit from scrip
     camera.stop_preview() # Switch off the camera
