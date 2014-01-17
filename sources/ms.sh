96e07cd5d8e05e2d3a8fa73ee1406494d9642930
BENCSIK Janos <git@butyi.hu>
2014-01-17 22:17:42 +0100

    folder name is date

--- sources/ms.sh
+++ sources/ms.sh
@@ -1,45 +1,72 @@
 #!/bin/bash
 # Movie digitalizer scipt. ms => Movie Scan
-display_usage() { 
-	echo "Movie digitalizer scipt" 
-	echo -e "\nUsage:\n$0 [bw|color] \n" 
+
+#Functions
+display_usage() {
+	echo -e "Movie digitalizer scipt\n"
+	echo -e "Usage:\n$0 [bw|color]\n"
 }
-# if less than two arguments supplied, display usage 
-if [  $# -le 1 ] 
-then 
-	display_usage
+
+line() {
+        echo -e "------------------------------------\n"
+}
+
+# Variables
+DATE=`date +%Y-%m-%d`
+
+# if less than two arguments supplied, display usage
+if [  $# -le 1 ]
+then
+        echo -e "Missing parameter!\n"
+ 	display_usage
 	exit 1
-fi 
- 
-# check whether user had supplied -h or --help . If yes display usage 
-if [[ ( $# == "--help") ||  $# == "-h" ]] 
-then 
+fi
+
+# check whether user had supplied -h or --help . If yes display usage
+if [[ ( $# == "--help") ||  $# == "-h" ]]
+then
 	display_usage
 	exit 0
-fi 
+fi
 
 # create folder for images if does not yet exist
-if [ ! -d ~/pics ]
+line
+if [ ! -d ~/$DATE ]
 then
-	mkdir ~/pics
+        echo -e "Step 1: Creating folder"+$DATE+"\n"
+	mkdir ~/$DATE # Create folder
+        cd ~/$DATE   # Jump to images folder
+else
+        echo -e "Step 1: Erase content of folder "+$DATE+"\n"
+        cd ~/$DATE   # Jump to images folder
+        sudo rm *    # Clear the folder
 fi
 
 # Jump to images folder
-cd ~/pics
-
-# clear content of folder
-rm ~/pics/*
+cd ~/$DATE
 
 # Shoot slides of movie. The sript exits at the end of movie automaticly
+line
+echo -e "Step 2: Shooting slides\n"
 sudo python ~/movscan/sources/py/movscan.py
 
 # If the movie is Black & White (bw), than convering images to grayscale
-if [  $1 == "bw" ] 
-then 
-	find *.jpg -exec convert '{}' -type Grayscale '{}' \;
-fi 
+line
+if [  $1 == "bw" ]
+then
+        echo -e "Step 3: Converting images to grayscale\n"
+	find *.jpg -exec sudo convert '{}' -type Grayscale '{}' \;
+fi
+if [  $1 == "color" ]
+then
+        echo -e "Step 3: Applying auto-white-balance on images\n"
+        find *.jpg -exec ~/mytools/bash/autowhite.sh '{}' '{}' \;
+fi
 
 # Create video from images
-avconv -r 15 -f image2 -i image%05d.jpg -crf 15 -b 10M -preset slower ~/video.avi
+line
+echo -e "Step 4: Merging images to video\n"
+avconv -r 15 -f image2 -i image%05d.jpg -crf 15 -b 10M -preset slower ~/$DATE/video-$DATE.avi
 
+echo -e "O.K. Video is available in ~/"+$DATE+" folder\n"
 
