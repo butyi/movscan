f9b8226bd6a10d47afa86dbb73dd99623254c502
BENCSIK Janos <git@butyi.hu>
2014-01-31 18:43:08 +0000

    crop modified

--- sources/py/movscan.py
+++ sources/py/movscan.py
@@ -92,7 +92,7 @@ with picamera.PiCamera() as camera:
     if "-s" in arguments: # For super film
         camera.crop = 0.27,0.19,0.6,0.6 # Crop active CCD part
     if "-n" in arguments: # For normal film
-        camera.crop = 0.35,0.23,0.50,0.50 # Crop active CCD part
+        camera.crop = 0.36,0.23,0.50,0.50 # Crop active CCD part
     camera.vflip = True # Mirroring is needed due to optic
     camera.preview_fullscreen = False # To be able to resize manually to see command line behind
     camera.preview_window = 600, 10, 640, 480 # It depends on your monitor resolution!
